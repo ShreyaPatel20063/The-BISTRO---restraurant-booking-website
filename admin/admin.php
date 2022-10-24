73eed1c40ccbfb42a08f86d048210ff338301773
ShreyaPatel20063 <sap20063@gmail.com>
2022-10-24 06:03:32 +0530

    admin dashboard added

---
+++ admin/admin.php
@@ -0,0 +1,47 @@
+<div>
+  <link href="./admin.css" rel="stylesheet" />
+  <header class="admin-container">
+    <header data-role="Accordion" class="admin-header">
+      <img
+        alt="logo"
+        src="/playground_assets/white%20logo%20-%20no%20background-200h.png"
+        class="admin-image"
+      />
+      <nav class="admin-nav"></nav>
+      <div data-type="AccordionHeader" class="admin-accordion-header">
+        <svg viewBox="0 0 1024 1024" class="admin-icon">
+          <path
+            d="M128 554.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667zM128 298.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667zM128 810.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667z"
+          ></path>
+        </svg>
+      </div>
+      <div data-type="AccordionContent" class="admin-accordion-content">
+        <div class="admin-nav1">
+          <nav class="navigation-links-nav navigation-links-root-class-name20">
+            <span class="navigation-links-text"><span>About</span></span>
+            <span class="navigation-links-text1"><span>Features</span></span>
+            <span class="navigation-links-text2"><span>Pricing</span></span>
+            <span class="navigation-links-text3"><span>Team</span></span>
+            <span class="navigation-links-text4"><span>Blog</span></span>
+          </nav>
+        </div>
+      </div>
+    </header>
+    <a
+      href="https://google.com"
+      target="_blank"
+      rel="noreferrer noopener"
+      class="admin-link button"
+    >
+      USER LIST
+    </a>
+    <a
+      href="https://google.com"
+      target="_blank"
+      rel="noreferrer noopener"
+      class="admin-link1 button"
+    >
+      ORDER LIST
+    </a>
+  </header>
+</div>
--- admin/usertable.php
+++ admin/usertable.php
@@ -1,41 +1,65 @@
 <!DOCTYPE html>
 <html>
-<style>
-table, th, td 
-{
-  border:1px solid black;
-}
-td{
-    padding: 10px;
-}
-body{
-    background-color: rgb(143, 176, 143);
-}
-</style>
-<body>
+  <style>
+    table, th, td 
+    {
+      border:1px solid black;
+    }
+    td{
+        padding: 10px;
+    }
+    body{
+        background-color: rgb(143, 176, 143);
+    }
+  </style>
+  <body>
 
-<h2>User Details table</h2>
+    <h2>User Details table</h2>
 
-<table style="width:100%">
-  <tr>
-   
-    <th>cid</th>
-    <th>email</th>
-    <th>phone</th>
-    <th>pswd</th>
-</tr>
-  <tr>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
-    
-  </tr>
-  
-</table>
+    <table style="width:100%">
+      <tr>
+      
+        <th>cid</th>
+        <th>email</th>
+        <th>phone</th>
+        <th>pswrd</th>
+
+      </tr>
+      <?php
+        include("location: ../dbconnection.php");
+
+        $selectqry = "SELECT * FROM tblcustomer";
+
+        $connecttoDB = mysqli_query($conn, $selectqry);
+        if($connecttoDB){
+          if($connecttoDB->num_rows > 0){
+            $data = mysqli_fetch_assoc($connecttoDB); 
+            while($data){
+              $cid = $data['cid'];
+              $email = $data['email'];
+              $phone = $data['phone'];
+              $pswrd = $data['pswrd'];
+            }
+          }
+          else{
+            echo "Query does not run!!";
+          }
+        }
+      ?>
+      
+
+        <td><?php echo $cid; ?></td>
+        <td><?php echo $email; ?></td>
+        <td><?php echo $phone; ?></td>
+        <td><?php echo $pswrd; ?></td>
+
+      </tr>
+      
+      
+    </table>
 
 
 
-</body>
+  </body>
 </html>
 
